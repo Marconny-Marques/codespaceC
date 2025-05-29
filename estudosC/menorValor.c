74bcdaa594bedb026f0a3062b8695893ed9210f7
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2025-05-29 14:33:18 +0000

    another changes

--- estudosC/menorValor.c
+++ estudosC/menorValor.c
@@ -5,26 +5,27 @@ int main(){
     int num2;
     int num3;
     int menor;
-    int cont;
+    int cont = 1;
 
-    while(cont!=0) {
-        printf("Digite o valor 1", &num2);
-        scanf("%f", &num1);
+    while(cont == 1) {
+        printf("Digite o valor 1", &num1);
+        scanf("%d", &num1);
 
         printf("Digite o valor 2", &num2);
-        scanf("%f", &num2);
+        scanf("%d", &num2);
 
         printf("Digite o valor 3", &num3);
-        scanf("%f", &num3);
+        scanf("%d", &num3);
 
-        if(num1< num2 && num3) {
+        if(num1 <= num2 && num1 <= num3) {
             menor = num1;
-            printf("O menor valor é: %d", num1);
-        } else if (num2 < num1 && num3){
+        } else if (num2 <= num1 && num2 <= num3){
             menor = num2;
         } else {
             menor = num3;
         }
+
+        printf("O menor valor é %d\n", menor);
     }
 
     return 0;
